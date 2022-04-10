3996341065c745a8abfa33188d4d17c1f168c8ca
mu-suke <chelsea_08ym@yahoo.co.jp>
2022-04-10 20:12:13 +0900

    lint, fmtの実行

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
