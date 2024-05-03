af5c5f01126a3638c39e2cef52657dcc8bb723de
mu-suke <45083522+mu-suke@users.noreply.github.com>
2024-05-03 19:22:28 +0900

    husky&lint-stagedからlefthook&biomeに置き換える (#45)
    * feat: biome&lefthookの導入
    
    * feat: biome&lefthookの導入
    
    * test
    
    * test
    
    * chore: eslint, prettierパッケージの削除

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -7,7 +7,7 @@ type Data = {
 export const runtime = 'edge'
 
 export default function handler(
-  req: NextApiRequest,
+  _req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
   res.status(200).json({ name: 'John Doe' })
